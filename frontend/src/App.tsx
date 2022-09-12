024994d7b35d739af1059c828ef6b25d77f546e3
Lucas Almeida <lucasav0512@gmail.com>
2022-09-11 23:22:45 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./componentes/header"
 import SalesCard from "./componentes/SalesCard"
 
 function App() {
   return(
     <>
+    <ToastContainer />
       <Header/>
       <main>
         <section id="sales">
